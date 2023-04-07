004a12fd013ab3ef08fdacdb6fdf5cd9f959e8d9
Aakaash J <aak.jamad@gmail.com>
2023-04-07 17:30:13 -0500

    Starting Position

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import tkinter as tk
-p = 1
-n = 2
-b = 3
-q = 4
-ch = 5
-ca = 6
-k = 7
+
+# Pawn = 1
+# Kight = 2
+# Bishop = 3
+# Queen = 4
+# Chancellor = 5
+# Camel1 = 6
+# Camel2 = 8
+# King = 7
 row0 =    [0,0,0,0,0]
 row1 =   [0,0,0,0,0,0]
-row2 =  [0,0,0,2,0,0,0]
+row2 =  [0,0,0,0,0,0,0]
 row3 = [0,0,0,0,0,0,0,0]
 row4 =[0,0,0,0,0,0,0,0,0]
 row5 = [0,0,0,0,0,0,0,0]
-row6 =  [0,0,0,2,0,0,0]
-row7 =   [0,0,0,0,0,0]
-row8 =    [0,0,0,0,0]
+row6 =  [2,0,0,0,0,0,2]
+row7 =   [1,3,1,1,3,1]
+row8 =    [6,5,7,4,8]
 rowcolumn = [row0,row1,row2,row3,row4,row5,row6,row7,row8]
+
+
 def identifypiece(location):
     if  location == 1:
         return "pawn"
@@ -31,22 +35,33 @@ def identifypiece(location):
         return "camel"
     elif location == 7:
         return "king"
+    elif location == 8:
+        return "camel"    
+
 def capture(capturer, captured):
     rowcolumn[(int(loc2[0])-1)][(int(loc2[2])-1)] = rowcolumn[(int(loc1[0])-1)][(int(loc1[2])-1)]
     rowcolumn[(int(loc1[0])-1)][(int(loc1[2])-1)] = 0
+
+
 while True:
     loc1 = str(input("What original location? "))
+
     if rowcolumn[(int(loc1[0])-1)][(int(loc1[2])-1)] == 0:
         print("There is no piece there!")
+    
     else:
         print("There is a " + str(identifypiece(rowcolumn[(int(loc1[0])-1)][(int(loc1[2])-1)])) + " there.")
 
         loc2 = str(input("What new location? "))
+
+
         if rowcolumn[(int(loc2[0])-1)][(int(loc2[2])-1)] == 0:
             rowcolumn[(int(loc1[0])-1)][(int(loc1[2])-1)],rowcolumn[(int(loc2[0])-1)][(int(loc2[2])-1)] = rowcolumn[(int(loc2[0])-1)][(int(loc2[2])-1)],rowcolumn[(int(loc1[0])-1)][(int(loc1[2])-1)]
             print("The " + str(identifypiece(rowcolumn[(int(loc1[0])-1)][(int(loc1[2])-1)])) + " at " + loc1 + " moved to " + loc2 + ".")
+        
         else:
             capture(rowcolumn[(int(loc1[0])-1)][(int(loc1[2])-1)], rowcolumn[(int(loc2[0])-1)][(int(loc2[2])-1)])
             print("The " + str(identifypiece(rowcolumn[(int(loc1[0])-1)][(int(loc1[2])-1)])) + " at " + loc1 + " captured the " + str(identifypiece(rowcolumn[(int(loc2[0])-1)][(int(loc2[2])-1)])) + " at " + loc2)
+        
         for i in range(9):
             print(rowcolumn[i])
